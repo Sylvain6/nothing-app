68b789461dacbfc37aae27e06f028e4c2ae125d9
Sylvain Coutrot <s.coutrot.ext_azu@sodexo.com>
2019-03-26 00:39:06 +0100

    add cards call and display

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import axios from 'axios';
-import AutoSuggest from 'react-autosuggest';
+import Button from "@material-ui/core/Button/Button";
+import AutoSuggest from "react-autosuggest";
 
 class Landing extends React.Component{
     state = {
         value: '',
+        cards: [],
         sets: [],
         suggestions: [],
     };
@@ -20,14 +22,14 @@ class Landing extends React.Component{
 
     getSuggestionValue = suggestion => suggestion.name;
 
-    onChange = (event, { newValue }) => {
+    onChange = (event, {newValue}) => {
         this.setState({
             value: newValue
         });
     };
 
     renderSuggestion = suggestion => (
-            suggestion.name
+        suggestion.name
     );
 
     onSuggestionsClearRequested = () => {
@@ -40,32 +42,52 @@ class Landing extends React.Component{
         axios.get('https://api.magicthegathering.io/v1/sets').then(res => this.setState({sets: res.data.sets}));
     };
 
-    onSuggestionsFetchRequested = ({ value }) => {
+    onSuggestionsFetchRequested = ({value}) => {
         this.setState({
             suggestions: this.getSuggestions(value)
         });
     };
 
-    render() {
-        const { suggestions, value } = this.state;
+    findCarsBySet = () => {
+        axios.get(`https://api.magicthegathering.io/v1/cards?setName=${this.state.value}`)
+            .then(response => {
+                console.log(response.data.cards);
+                this.setState({cards: response.data.cards})
+            })
+    };
+
+    render(){
+        const { cards, suggestions, value } = this.state;
 
         const inputProps = {
             placeholder: 'Type a set',
             value,
-            onChange: this.onChange
+            onChange: this.onChange,
         };
-    return(
-        <>
-            <AutoSuggest
-                suggestions={suggestions}
-                onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-                onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-                getSuggestionValue={this.getSuggestionValue}
-                renderSuggestion={this.renderSuggestion}
-                inputProps={inputProps}
-            />
-        </>
-    )}
+        return(
+            <>
+
+                <AutoSuggest
+                    suggestions={suggestions}
+                    onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                    onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                    getSuggestionValue={this.getSuggestionValue}
+                    renderSuggestion={this.renderSuggestion}
+                    inputProps={inputProps}
+                />
+                <Button type="submit" className="primary" onClick={this.findCarsBySet} style={{
+                backgroundColor: '#B793E6',
+                color:"white",
+                margin:"8px"
+                }}>
+                    Search
+                </Button>
+                {cards && cards.map(card =>
+                    <img src={card.imageUrl} />
+                )}
+            </>
+        )
+    }
 };
 
 
